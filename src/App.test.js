2a5216abddc3d1650e08a941ca4536a1b8e2be5b
yjcho <jinjanic91@gmail.com>
2022-05-22 23:44:56 +0900

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -5,4 +5,8 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
+  // lint 설정 잘 되었는지 테스트
+  // const lintTest = screen.getByRole('button', { name: 'lintTest' });
+  // expect(lintTest).toHaveTextContent('lintTest');
 });
