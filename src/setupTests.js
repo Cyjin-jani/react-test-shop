c4fb213aad590dcf488baafa044d470ac0a30f3d
yjcho <jinjanic91@gmail.com>
2022-05-24 22:49:26 +0900

    feat. mock service worker done

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import { server } from './mocks/server';
+
+// test실행 전 서버를 켬
+beforeAll(() => server.listen());
+
+// 각 테스트 끝나면 서버의 핸들러 리셋
+afterEach(() => server.resetHandlers());
+
+// test가 모두 끝나면 서버를 꺼준다.
+afterAll(() => server.close());
