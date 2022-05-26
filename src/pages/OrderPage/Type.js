ca780263ca63a93dd160afe76384ad0a786faf44
yjcho <jinjanic91@gmail.com>
2022-05-26 23:52:40 +0900

    feat. error banner test done

--- src/pages/OrderPage/Type.js
+++ src/pages/OrderPage/Type.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Products from './Products';
+import ErrorBanner from '../../components/ErrorBanner';
 
 const Type = ({ orderType }) => {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     loadItems(orderType);
@@ -14,9 +16,15 @@ const Type = ({ orderType }) => {
       let response = await axios.get(`http://localhost:5000/${orderType}`);
       setItems(response.data);
     } catch (error) {
-      console.error(error);
+      // console.error(error);
+      setError(true);
     }
   };
+
+  if (error) {
+    return <ErrorBanner message="에러가 발생했습니다." />;
+  }
+
   const ItemComponents = orderType === 'products' ? Products : null;
 
   const optionItems = items.map((item) => (
--- src/pages/OrderPage/tests/Type.test.js
+++ src/pages/OrderPage/tests/Type.test.js
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Type from '../Type';
+import { server } from '../../../mocks/server';
+import { rest } from 'msw';
 
 test('display product images from server', async () => {
   render(<Type orderType="products" />);
@@ -12,4 +14,18 @@ test('display product images from server', async () => {
 
   const altText = productImages.map((el) => el.alt);
   expect(altText).toEqual(['America product', 'England product']);
+});
+
+test('when face an error fetching datas , display errors', async () => {
+  // server를 리셋해서 정상적인 배열의 데이터 대신에 500error를 보내주도록 합니다.
+  server.resetHandlers(
+    rest.get('http://localhost:5000/products', (req, res, ctx) => {
+      return res(ctx.status(500));
+    })
+  );
+
+  render(<Type orderType="products" />);
+
+  const errorBanner = await screen.findByTestId('error-banner');
+  expect(errorBanner).toHaveTextContent('에러가 발생했습니다.');
 });
