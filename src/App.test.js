161c799fdc8319b8dde6635886705e60fa49feef
yjcho <jinjanic91@gmail.com>
2022-06-07 20:47:33 +0900

    feat. 주문하기 test done

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test('renders learn react link', () => {
@@ -8,4 +9,33 @@ test('renders learn react link', () => {
   // lint 설정 잘 되었는지 테스트
   // const lintTest = screen.getByRole('button', { name: 'lintTest' });
   // expect(lintTest).toHaveTextContent('lintTest');
+});
+
+test('From order to order complete page', async () => {
+  // 이미 App.js에서 context를 감싸주고 있기 때문에
+  // CustomRender 메서드가 아닌 기본 라이브러리 내장 render 메서드를 사용.
+  render(<App />);
+
+  const americaInput = await screen.findByRole('spinbutton', {
+    name: 'America',
+  });
+
+  userEvent.clear(americaInput);
+  userEvent.type(americaInput, '2');
+
+  const englandInput = await screen.findByRole('spinbutton', {
+    name: 'England',
+  });
+
+  userEvent.clear(englandInput);
+  userEvent.type(englandInput, '3');
+
+  const insuranceCheckbox = await screen.findByRole('checkbox', {
+    name: 'Insurance',
+  });
+
+  userEvent.click(insuranceCheckbox);
+
+  const orderBtn = screen.getByRole('button', { name: '주문하기' });
+  userEvent.click(orderBtn);
 });
--- src/pages/OrderPage/OrderPage.js
+++ src/pages/OrderPage/OrderPage.js
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { OrderContext } from '../../contexts/OrderContext';
 import Type from './Type';
 
-const OrderPage = () => {
+const OrderPage = ({ setStep }) => {
   const [orderDatas] = useContext(OrderContext);
 
   return (
@@ -18,7 +18,7 @@ const OrderPage = () => {
         <div>
           <h2>Total Price: {orderDatas.totals.total}</h2>
           <br />
-          <button>주문</button>
+          <button onClick={() => setStep(1)}>주문하기</button>
         </div>
       </div>
     </div>
